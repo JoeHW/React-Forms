1662b7781e9827b2172f4699f3995c18dba55b74
Joe Williamson <jhwilliamson@outlook.com>
2017-08-16 12:41:15 -0400

    Card component

--- GHCards.js
+++ GHCards.js
@@ -1,3 +1,14 @@
-const Card = ...
+const Card = (props) => {
+  return (
+    <div>
+       <img src="http://placeholder.it/75" />
+       <div>
+         <div>Name here...</div>
+         <div>Company Name here...</div>
+        </div>
+    </div>
 
-const CardList = ...
+  );
+};
+
+ReactDOM.render(<Card />, mountNode);
